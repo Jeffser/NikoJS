fa24b67292f4177ac3137d731530866138318248
Jeffry Samuel Eduarte Rojas <69224322+Jeffser@users.noreply.github.com>
2022-05-15 20:51:53 -0600

    Update niko.js

--- niko.js
+++ niko.js
@@ -1,19 +1,13 @@
 var mousePos;
 document.onmousemove = handleMouseMove;
-setInterval(getMousePosition, 100); // setInterval repeats every X ms
+setInterval(getMousePosition, 100);
 function handleMouseMove(event) {
     var dot, eventDoc, doc, body, pageX, pageY;
-
-    event = event || window.event; // IE-ism
-
-        // If pageX/Y aren't available and clientX/Y are,
-        // calculate pageX/Y - logic taken from jQuery.
-        // (This is to support old IE)
+    event = event || window.event;
     if (event.pageX == null && event.clientX != null) {
         eventDoc = (event.target && event.target.ownerDocument) || document;
         doc = eventDoc.documentElement;
         body = eventDoc.body;
-
         event.pageX = event.clientX +
             (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
             (doc && doc.clientLeft || body && body.clientLeft || 0);
@@ -21,7 +15,6 @@ function handleMouseMove(event) {
             (doc && doc.scrollTop  || body && body.scrollTop  || 0) -
             (doc && doc.clientTop  || body && body.clientTop  || 0 );
     }
-
     mousePos = {
         x: event.pageX,
         y: event.pageY
